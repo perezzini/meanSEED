471791b1ebd61c418a0efcacbadaf32433135b6f
Luciano Perezzini <lperezzini@gmail.com>
2017-01-06 11:40:24 -0300

    client: app: app.component

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 
 @Component({
-	moduleId: module.id, // use this in order to use relative paths
+	moduleId: module.id,
   selector: 'my-app',
-  //template: `<h1>Hello {{name}}</h1>`,
   templateUrl: 'app.component.html'
 })
+
 export class AppComponent  { 
-	name: string = 'Angular';
-	description: string = 'Start from here! This is the root component!';
+	title: string;
+	description: string;
+	url: string;
+
+	constructor() {
+		this.title = 'meanSEED';
+		this.description = 'Start from here! This is the root component!';
+		this.url = 'https://github.com/perezzini/meanSEED';
+	}
 
 }
