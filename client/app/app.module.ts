6be8d437663c563bb19f900b5a384104b69df79b
Luciano Perezzini <lperezzini@gmail.com>
2017-01-06 11:33:37 -0300

    client: app: app.module.ts: import ChildComponent

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { ChildComponent }  from './components/child.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, ChildComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
