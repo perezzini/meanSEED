b2b5c33a7a5ca2346391e773faecb45b58ad020d
Luciano Perezzini <lperezzini@gmail.com>
2017-01-06 15:54:06 -0300

    Error fix when connecting to db

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ require('./models/thing');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://' + 
 				process.env.DB_HOST + 
+        ':' + 
+        process.env.DB_PORT + 
 				'/' + 
 				process.env.DB_NAME);
 
