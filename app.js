9ec83e74a155cc301d39185a263c9503d55d4887
Luciano Perezzini <lperezzini@gmail.com>
2017-01-09 11:16:22 -0300

    Handlebars config

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var hbs = require('express-handlebars');
+
 // init Mongoose schemas & init db through Mongoose
 require('./models/thing');
 var mongoose = require('mongoose');
@@ -21,10 +23,9 @@ var api = require('./routes/api');
 var app = express();
 
 // view engine setup
+app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/'}));
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-// better check out this...
-app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
